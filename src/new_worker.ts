57117dc716db1feb95ccc2ad3a4b384bb45854ab
Jesko Dujmovic <jeskod+git@gmail.com>
2019-02-19 22:54:30 +0100

    fixing worker again

--- src/new_worker.ts
+++
@@ -1,3 +0,0 @@
-export default function () {
-    return new Worker('./worker.js');
-}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/worker.ts',
+  output: {
+    filename: 'webworker.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
